279ccd2c3d39f531f60ca7c23e04769fa447389c
N!hal <71655709+amoreX@users.noreply.github.com>
2025-08-14 19:59:04 +0530

    Update customiseRoadmap.js

--- app/utils/customiseRoadmap.js
+++ app/utils/customiseRoadmap.js
@@ -7,7 +7,7 @@ export async function getCustom(custom, roadmap) {
     "AIzaSyAxWhVJfD7fw1RLuP86qNMbflH2N7gcchY"
   );
   //new one -> AIzaSyBYm_TyKpW2rrhyOZSekvc1BlUP9_SKJYA
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   let prompt = `modify the given roadmap: ${roadmap} according to the prompt: ${custom} ,only respond in JSON nothing else,do not give any other text , so that i can use JSON function to convert your response to JSON ,json structure should be identical to given roadmap`;
 
